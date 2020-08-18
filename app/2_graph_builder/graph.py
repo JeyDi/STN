1a78addedca0a7a4fb2d2acd625fe37a73c10b88
manuelzanaboni <mzana97@gmail.com>
2020-08-18 16:53:13 +0200

    update graph.py

--- app/2_graph_builder/graph.py
+++ app/2_graph_builder/graph.py
@@ -1,27 +1,41 @@
 import networkx as nx
 import pandas as pd
 
-
-''' Import tweets dataset '''
+''' Import followers dataset '''
 df = pd.read_csv(r'../1_scraper/conte_followers.csv').iloc[:200]
 
+print('Direct graph? y/n')
+val = str(input())
 
-''' Create direct graph '''
-G = nx.DiGraph()
-G.add_node('GiuseppeConteIT')
+''' Create graph '''
+try:
+    G = None
 
-for u in df['username']:
-    try: 
-        level2 = pd.read_csv(r'../1_scraper/conte_followers/'+u+'_followers.csv')
-        G.add_node(u)  
-        G.add_edge(u, 'GiuseppeConteIT')
+    if val == 'y':
+        G = nx.DiGraph()
+    elif val == 'n':
+        G = nx.Graph()
+        
+    G.add_node('GiuseppeConteIT')
+
+    for u in df['username']:
+        try: 
+            level2 = pd.read_csv(r'../1_scraper/conte_followers/'+ u +'_followers.csv')
+            G.add_node(u)  
+            G.add_edge(u, 'GiuseppeConteIT')
+        
+            for v in level2['username']:
+                G.add_node(v)
+                G.add_edge(v, u)
+        except:
+            print(u + ' Not downloaded')
+        
+    G_int = nx.convert_node_labels_to_integers(G)
+
+    if val == 'y':
+        nx.write_gexf(G_int, './graph_200_int_direct.gexf')
+    elif val == 'n':
+        nx.write_gexf(G_int, './graph_200_int.gexf')
     
-        for v in level2['username']:
-            G.add_node(v)
-            G.add_edge(v, u)
-    except:
-        print(u + ' Not downloaded')
-    
-
-G_int = nx.convert_node_labels_to_integers(G)
-nx.write_gexf(G_int, './graph_200_int_direct.gexf')
+except:
+    print('INPUT ERROR')
