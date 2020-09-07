c1d0008183d841560195f55f32ad640c0b6f05fb
vittoriomaggio <posta.maggio@gmail.com>
2020-09-07 16:52:26 +0200

    Add chart

--- app/5_statistics/stats.py
+++ app/5_statistics/stats.py
@@ -30,11 +30,12 @@ def print_stats(G):
 
 
 if __name__ == '__main__':
+    
+    df_final_exposed = pd.DataFrame(columns=['bot', 'exposed'])
     for val in ['random', 'btw', 'eigenvector']:
         print(f'PRINTING {val} GRAPH STATS')
         df_step = pd.DataFrame(columns=['type', 'step', 'value'])
-        df_exposed = pd.DataFrame(columns=['step', 'type' , 'value'  ])
-
+        df_exposed = pd.DataFrame(columns=['step', 'type' , 'value'])
         for i in range(5): 
             G = nx.read_gexf(f'../4_visualization/output_graph/{val}/G_{val}_step{i}.gexf')
             print(f'STEP {i}:')
@@ -48,16 +49,18 @@ if __name__ == '__main__':
             line_chart = px.line(df_step, x='step', y='value', color='type', title=val)
             
             #Bar chart
-            df_exposed = df_exposed.append({'step' : i, 'type' : 'opinion_leader', 'value' : cn.count_exposed_opinion_leader(G)},ignore_index=True)
-            df_exposed = df_exposed.append({'step' : i, 'type' : 'bot', 'value' : cn.count_exposed_bot(G)},ignore_index=True)
-            df_exposed = df_exposed.append({'step' : i, 'type' : 'user', 'value' : cn.count_exposed_user(G)},ignore_index=True)
+            df_exposed = df_exposed.append({'step' : i, 'type' : 'opinion_leader', 'value' : cn.count_exposed_opinion_leader(G)}, ignore_index=True)
+            df_exposed = df_exposed.append({'step' : i, 'type' : 'bot', 'value' : cn.count_exposed_bot(G)}, ignore_index=True)
+            df_exposed = df_exposed.append({'step' : i, 'type' : 'user', 'value' : cn.count_exposed_user(G)}, ignore_index=True)
             bar_chart = px.bar(df_exposed, x='step', y = 'value', color = 'type', title = val )
             
-
-            
+        df_final_exposed = df_final_exposed.append({'bot' : val, 'exposed' : cn.count_exposed(G)}, ignore_index=True)
         plot(line_chart,   filename=f'./charts/steps/{val}.html')
         plot(bar_chart,   filename=f'./charts/exposed_type/{val}.html')
-         
-        
+
         print()
+        
+    bar_chart = px.bar(df_final_exposed, x='bot', y='exposed')
+    plot(bar_chart, filename=f'./charts/final_exposed/final_exposed.html')
+
         
