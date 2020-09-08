c07b4e3b92a32e46398566fd5cc2ce94b751ab1f
Andrea Guzzo <andrea.guzzo92@gmail.com>
2020-09-08 11:04:31 +0200

    small fix

--- streamlit_app/scraper/twitter.py
+++ streamlit_app/scraper/twitter.py
@@ -2,26 +2,27 @@ import twint
 import nest_asyncio
 import pandas as pd
 
-#nest_asyncio.apply()
+# nest_asyncio.apply()
 
 # print("Download Conte's followers? y/n")
 # val = str(input())
 
-def config(username = "GiuseppeConteIT", store = True, output = "./conte_followers.csv"):
+
+def config(username="GiuseppeConteIT", store=True, output="./data/conte_followers.csv"):
     """
     Configure Twint settings for scraping
     """
     c = twint.Config()
     c.Username = username
     c.Store_csv = store
-    c.Output=(output)
-    
+    c.Output = output
+
     print(f"Configuration set: {c}")
-    
+
     return c
 
 
-def download(config, level = 1):
+def download(config, output_path, level=1):
     """
     Download new tweets using configuration
     """
@@ -35,21 +36,21 @@ def download(config, level = 1):
             return False
     elif level == 2:
         print(f"Downloading level 2")
-        conte_df = pd.read_csv('./conte_followers.csv').iloc[1950:2000]
-        error_list=[]
+        conte_df = pd.read_csv(output_path).iloc[1950:2000]
+        error_list = []
         c = twint.Config()
-        for user in conte_df['username']:
+        for user in conte_df["username"]:
             try:
                 c.Username = user
                 c.Store_csv = True
-                c.Output=("./conte_followers/"+user+"_followers.csv")
+                c.Output = "./conte_followers/" + user + "_followers.csv"
                 twint.run.Followers(c)
-                print(f"{user} downloaded") 
+                print(f"{user} downloaded")
                 return True
             except Exception as message:
                 print(f"Error downloading: {user}, {message}")
-                error_list.append(user) 
-                return False 
+                error_list.append(user)
+                return False
     else:
         print(f"Please specify a graph level: 1 or 2 in the function")
         return False
