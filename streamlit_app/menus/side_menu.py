aa46dde735e7879731c7e32cab05683d66c20a98
Manuel <60094377+manuelzanaboni@users.noreply.github.com>
2020-09-26 15:37:32 +0200

    Update Graph build - streamlit app

--- streamlit_app/menus/side_menu.py
+++ streamlit_app/menus/side_menu.py
@@ -65,12 +65,21 @@ def menu_graph_generator():
         "Dataset Path", value="./data/conte_followers.csv"
     )
     follower_number = st.sidebar.number_input(
-        "Level of graph distance", min_value=1, max_value=1000, value=500
+        "Level of graph distance", min_value=1, max_value=2000, value=500
     )
     level2_path = st.sidebar.text_input(
         "Level 2 followers path", value="./data/conte_followers"
     )
-    graph_name = st.sidebar.text_input("Graph name", value="500-users")
+    graph_name_list = [
+        "500-users",
+        "1000-users",
+        "1500-users",
+        "2000-users"
+    ]
+    graph_name = st.sidebar.selectbox(
+        "Graph name", graph_name_list
+    )
+
     graph_direct = st.sidebar.checkbox(
         "Check the box if you want to generate a direct graph", True
     )
@@ -82,9 +91,9 @@ def menu_graph_generator():
             # load the dataframe
             with st.spinner("Start creating the graph...please be patient.."):
 
-                df = pd.read_csv(dataset_path).iloc[:follower_number]
+                df = pd.read_csv(dataset_path)
                 # create the graph
-                result = create_graph(df, level2_path, graph_name, graph_direct)
+                result = create_graph(df, follower_number, level2_path, graph_name, graph_direct)
 
                 st.success(
                     f"Graph: **{graph_name}** created succesfully with: **{result}** number of nodes"
@@ -180,38 +189,9 @@ def menu_soil_simulation_subroutine():
 
                 # run the simulation
                 ### NON FUNZIONA E NON SO COME MAI!!!!!!
-<<<<<<< HEAD
                 # soil.simulation.run_from_config(configurations)
                 # soil.simulation.run_from_config(soil_config_path)
                 # soil.simulation.run_from_config('../simulation/spread_config.yml')
-=======
-                #soil.simulation.run_from_config(configurations)
-                #soil.simulation.run_from_config(soil_config_path)
-                soil.simulation.run_from_config('../simulation/spread_config.yml')
-                '''
-                ################################ INIZIO DEBUG
-                for (
-                    config_def
-                ) in configurations:  # CICLA SU STRINGHE (entry del dizionario)
-                    print(f"entry: {config_def}")
-                    # logger.info("Found {} config(s)".format(len(ls)))
-                    for config, path in load_config(
-                        config_def
-                    ):  # COPIATA FUNZIONE SOTTO, SCROLLA
-                        name = config.get("name", "unnamed")
-                        # logger.info("Using config(s): {name}".format(name=name))
-
-                        dir_path = config.pop(
-                            "dir_path", os.path.dirname(path)
-                        )  # CODICE CHE VA IN ERRORE
-                    """
-                    sim = Simulation(dir_path=dir_path,
-                                    **config)
-                    sim.run_simulation(**kwargs)
-                    """
-                '''
-                ################################ FINE DEBUG
->>>>>>> 6890fb7db53e558bd62360aee7d44e3a7ad26d0d
 
                 status = True
                 print(
